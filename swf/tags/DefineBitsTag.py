a84d3395838fa93ca6190a5090dfc7ecbb5cecec
Nat <56046513+NathaanTFM@users.noreply.github.com>
2023-01-21 17:40:25 +0100

    Ajout JPEGTables et DefineBits

--- swf/tags/DefineBitsTag.py
+++ swf/tags/DefineBitsTag.py
@@ -1,21 +1,34 @@
 from __future__ import annotations
 from swf.tags.Tag import Tag
 from swf.stream.SWFInputStream import SWFInputStream
+from swf.stream.SWFOutputStream import SWFOutputStream
 
 class DefineBitsTag(Tag):
     """
-    The ShowFrame tag instructs Flash Player to display the
-    contents of the display list. The file is paused for
-    the duration of a single frame.
+    This tag defines a bitmap character with JPEG compression.
     """
     tagId = 6
+
+    characterId: int
+    jpegData: bytes
+
+    def __init__(self, characterId: int, jpegData: bytes):
+        self.characterId = characterId
+        self.jpegData = jpegData
 
     @staticmethod
     def read(stream: SWFInputStream) -> Tag:
         if stream.version < 1:
             raise ValueError("bad swf version")
 
+        characterId = stream.readUI16()
+        jpegData = stream.read(stream.available())
+        return DefineBitsTag(characterId, jpegData)
 
-    def export(self, stream: SWFInputStream) -> None:
+
+    def write(self, stream: SWFOutputStream) -> None:
         if stream.version < 1:
             raise ValueError("bad swf version")
+
+        stream.writeUI16(self.characterId)
+        stream.write(self.jpegData)
--- swf/tags/JPEGTablesTag.py
+++ swf/tags/JPEGTablesTag.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from swf.tags.Tag import Tag
 from swf.stream.SWFInputStream import SWFInputStream
+from swf.stream.SWFOutputStream import SWFOutputStream
 
 class JPEGTablesTag(Tag):
     """
@@ -10,12 +11,23 @@ class JPEGTablesTag(Tag):
     """
     tagId = 8
 
+    jpegData: bytes
+
+    def __init__(self, jpegData: bytes):
+        self.jpegData = jpegData
+
+
     @staticmethod
     def read(stream: SWFInputStream) -> Tag:
         if stream.version < 1:
             raise ValueError("bad swf version")
 
+        jpegData = stream.read(stream.available())
+        return JPEGTablesTag(jpegData)
 
-    def export(self, stream: SWFInputStream) -> None:
+
+    def write(self, stream: SWFOutputStream) -> None:
         if stream.version < 1:
             raise ValueError("bad swf version")
+
+        stream.write(self.jpegData)
