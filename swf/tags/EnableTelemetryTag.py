1d14d2ca80cfcd0af04918e8fcf8e45f08026e83
NathaanTFM <56046513+NathaanTFM@users.noreply.github.com>
2023-06-25 17:08:20 +0200

    Ajout tag EnableTelemetry

--- swf/tags/EnableTelemetryTag.py
+++ swf/tags/EnableTelemetryTag.py
@@ -4,18 +4,45 @@ from swf.stream.SWFInputStream import SWFInputStream
 
 class EnableTelemetryTag(Tag):
     """
-    The ShowFrame tag instructs Flash Player to display the
-    contents of the display list. The file is paused for
-    the duration of a single frame.
+    Telemetry is a a Flash player feature that sends profiling information about the runtime and the currently 
+    running content. The EnableTelemetry tag controls whether the advanced features of telemetry are included in 
+    the profile data. If the tag isn’t present, only basic information is available.
     """
     tagId = 93
+    
+    passwordHash: bytes | None
+    
+    def __init__(self, passwordHash: bytes | None = None):
+        self.passwordHash = passwordHash
+        
 
     @staticmethod
     def read(stream: SWFInputStream) -> Tag:
         if stream.version < 1:
             raise ValueError("bad swf version")
+            
+        if stream.readUI16() != 0:
+            raise Exception("reserved is non-zero")
+            
+        avail = stream.available()
+        if avail == 0:
+            passwordHash = None
+        elif avail == 32:
+            passwordHash = stream.read(32)
+        else:
+            raise Exception("bad length for hash (%d)" % avail)
+            
+        return EnableTelemetryTag(passwordHash)
+            
 
-
-    def export(self, stream: SWFInputStream) -> None:
+    def write(self, stream: SWFInputStream) -> None:
         if stream.version < 1:
             raise ValueError("bad swf version")
+            
+        stream.writeUI16(0)
+        
+        if self.passwordHash is not None:
+            if len(self.passwordHash) != 32:
+                raise Exception("bad length for hash (%d)" % len(self.passwordHash))
+                
+            stream.write(self.passwordHash)
