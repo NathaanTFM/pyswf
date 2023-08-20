74bc9f15a70cdc43bbe0c24b652b1d3a21f385b0
NathaanTFM <56046513+NathaanTFM@users.noreply.github.com>
2023-08-20 21:24:01 +0200

    Ajout prototype par défaut Filter.write

--- swf/filters/Filter.py
+++ swf/filters/Filter.py
@@ -4,10 +4,15 @@ from typing import ClassVar
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from swf.stream.SWFInputStream import SWFInputStream
+    from swf.stream.SWFOutputStream import SWFOutputStream
 
 class Filter:
     id: ClassVar[int]
 
     @staticmethod
     def read(stream: SWFInputStream) -> Filter:
+        raise NotImplementedError()
+    
+
+    def write(self, stream: SWFOutputStream) -> None:
         raise NotImplementedError()
