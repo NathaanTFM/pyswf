180b7940f9440b872d47c87288a92fe52c00dcf9
NathaanTFM <56046513+NathaanTFM@users.noreply.github.com>
2023-08-23 20:12:23 +0200

    Ajout CSMTextSettingsTag

--- swf/tags/CSMTextSettingsTag.py
+++ swf/tags/CSMTextSettingsTag.py
@@ -11,16 +11,48 @@ class CSMTextSettingsTag(Tag):
     """
     tagId = 74
 
+    textId: int
+    useFlashType: int
+    gridFit: int
+    thickness: float
+    sharpness: float
+
+    def __init__(self, textId: int, useFlashType: int, gridFit: int, thickness: float, sharpness: float) -> None:
+        self.textId = textId
+        self.useFlashType = useFlashType
+        self.gridFit = gridFit
+        self.thickness = thickness
+        self.sharpness = sharpness
+
+
     @staticmethod
     def read(stream: SWFInputStream) -> Tag:
-        if stream.version < 1:
+        if stream.version < 8:
             raise ValueError("bad swf version")
         
-        raise NotImplementedError()
+        textId = stream.readUI16()
+        useFlashType = stream.readUB(2)
+        gridFit = stream.readUB(3)
+        if stream.readUB(3) != 0:
+            raise Exception("reserved is non-zero")
+        
+        thickness = stream.readFLOAT()
+        sharpness = stream.readFLOAT()
+        
+        if stream.readUI8() != 0:
+            raise Exception("reserved is non-zero")
+        
+        return CSMTextSettingsTag(textId, useFlashType, gridFit, thickness, sharpness)
 
 
     def write(self, stream: SWFOutputStream) -> None:
-        if stream.version < 1:
+        if stream.version < 8:
             raise ValueError("bad swf version")
         
-        raise NotImplementedError()
+        stream.writeUI16(self.textId)
+        stream.writeUB(2, self.useFlashType)
+        stream.writeUB(3, self.gridFit)
+        stream.writeUB(3, 0)
+        stream.writeFLOAT(self.thickness)
+        stream.writeFLOAT(self.sharpness)
+        stream.writeUI8(0)
