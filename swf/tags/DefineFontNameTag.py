82315218101c577c2dfd3b9f8676294522439fe1
NathaanTFM <56046513+NathaanTFM@users.noreply.github.com>
2023-08-23 22:22:06 +0200

    Ajout DefineFontNameTag

--- swf/tags/DefineFontNameTag.py
+++ swf/tags/DefineFontNameTag.py
@@ -5,22 +5,36 @@ from swf.stream.SWFOutputStream import SWFOutputStream
 
 class DefineFontNameTag(Tag):
     """
-    The ShowFrame tag instructs Flash Player to display the
-    contents of the display list. The file is paused for
-    the duration of a single frame.
+    The DefineFontName tag contains the name and copyright information
+    for a font embedded in the SWF file.
     """
     tagId = 88
 
+    fontId: int
+    fontName: str
+    fontCopyright: str
+
+    def __init__(self, fontId: int, fontName: str, fontCopyright: str) -> None:
+        self.fontId = fontId
+        self.fontName = fontName
+        self.fontCopyright = fontCopyright
+
+
     @staticmethod
     def read(stream: SWFInputStream) -> Tag:
-        if stream.version < 1:
+        if stream.version < 9:
             raise ValueError("bad swf version")
         
-        raise NotImplementedError()
+        fontId = stream.readUI16()
+        fontName = stream.readString()
+        fontCopyright = stream.readString()
+        return DefineFontNameTag(fontId, fontName, fontCopyright)
 
 
     def write(self, stream: SWFOutputStream) -> None:
-        if stream.version < 1:
+        if stream.version < 9:
             raise ValueError("bad swf version")
         
-        raise NotImplementedError()
+        stream.writeUI16(self.fontId)
+        stream.writeString(self.fontName)
+        stream.writeString(self.fontCopyright)
