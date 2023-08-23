ffeed239100e9731e48c5fe4091ebe71e24dd4dc
NathaanTFM <56046513+NathaanTFM@users.noreply.github.com>
2023-08-23 12:38:02 +0200

    Ajout tag ImportAssets2

--- swf/tags/ImportAssets2Tag.py
+++ swf/tags/ImportAssets2Tag.py
@@ -5,22 +5,51 @@ from swf.stream.SWFOutputStream import SWFOutputStream
 
 class ImportAssets2Tag(Tag):
     """
-    The ShowFrame tag instructs Flash Player to display the
-    contents of the display list. The file is paused for
-    the duration of a single frame.
+    The ImportAssets2 tag replaces the ImportAssets tag for SWF 8 and later.
+    ImportAssets2 currently mirrors the ImportAssets tag's functionality
     """
     tagId = 71
 
+    url: str
+    characters: list[tuple[int, str]]
+
+    def __init__(self, url: str, characters: list[tuple[int, str]]) -> None:
+        self.url = url
+        self.characters = characters
+
+
     @staticmethod
     def read(stream: SWFInputStream) -> Tag:
-        if stream.version < 1:
+        if stream.version < 8:
             raise ValueError("bad swf version")
         
-        raise NotImplementedError()
+        url = stream.readString()
+        if stream.readUI8() != 1: # downloadNow
+            raise Exception("reserved is not 1")
+        
+        if stream.readUI8() != 0: # hasDigest
+            raise Exception("reserved is not 0")
+        
+        count = stream.readUI16()
+        characters: list[tuple[int, str]] = []
+        
+        for _ in range(count):
+            tag = stream.readUI16()
+            name = stream.readString()
+            characters.append((tag, name))
+
+        return ImportAssets2Tag(url, characters)
 
 
     def write(self, stream: SWFOutputStream) -> None:
-        if stream.version < 1:
+        if stream.version < 8:
             raise ValueError("bad swf version")
         
-        raise NotImplementedError()
+        stream.writeString(self.url)
+        stream.writeUI8(1)
+        stream.writeUI8(0)
+
+        stream.writeUI16(len(self.characters))
+        for tag, name in self.characters:
+            stream.writeUI16(tag)
+            stream.writeString(name)
