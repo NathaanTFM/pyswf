f26a2dcbf46bd2a34c510cb402eb44adf7a04e2d
NathaanTFM <56046513+NathaanTFM@users.noreply.github.com>
2023-08-23 19:20:47 +0200

    Ajout DefineButtonCxformTag

--- swf/tags/DefineButtonCxformTag.py
+++ swf/tags/DefineButtonCxformTag.py
@@ -2,25 +2,36 @@ from __future__ import annotations
 from swf.tags.Tag import Tag
 from swf.stream.SWFInputStream import SWFInputStream
 from swf.stream.SWFOutputStream import SWFOutputStream
+from swf.records.ColorTransform import ColorTransform
 
 class DefineButtonCxformTag(Tag):
     """
-    The ShowFrame tag instructs Flash Player to display the
-    contents of the display list. The file is paused for
-    the duration of a single frame.
+    DefineButtonCxform defines the color transform for each shape
+    and text character in a button.
     """
     tagId = 23
 
+    buttonId: int
+    buttonColorTransform: ColorTransform
+    
+    def __init__(self, buttonId: int, buttonColorTransform: ColorTransform) -> None:
+        self.buttonId = buttonId
+        self.buttonColorTransform = buttonColorTransform
+
+
     @staticmethod
     def read(stream: SWFInputStream) -> Tag:
-        if stream.version < 1:
+        if stream.version < 2:
             raise ValueError("bad swf version")
         
-        raise NotImplementedError()
+        buttonId = stream.readUI16()
+        buttonColorTransform = stream.readCXFORM()
+        return DefineButtonCxformTag(buttonId, buttonColorTransform)
 
 
     def write(self, stream: SWFOutputStream) -> None:
-        if stream.version < 1:
+        if stream.version < 2:
             raise ValueError("bad swf version")
         
-        raise NotImplementedError()
+        stream.writeUI16(self.buttonId)
+        stream.writeCXFORM(self.buttonColorTransform)
