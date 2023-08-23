8f4f3b3a8807643fe99d018604de13139283971d
NathaanTFM <56046513+NathaanTFM@users.noreply.github.com>
2023-08-23 19:43:42 +0200

    Ajout ImportAssetsTag

--- swf/tags/ImportAssetsTag.py
+++ swf/tags/ImportAssetsTag.py
@@ -5,22 +5,45 @@ from swf.stream.SWFOutputStream import SWFOutputStream
 
 class ImportAssetsTag(Tag):
     """
-    The ShowFrame tag instructs Flash Player to display the
-    contents of the display list. The file is paused for
-    the duration of a single frame.
+    The ImportAssets tag imports characters from another SWF file.
+    The importing SWF file references the exporting SWF file by the URL where it can be found.
+    Imported assets are added to the dictionary just like characters defined within a SWF file
     """
     tagId = 57
 
+    url: str
+    characters: list[tuple[int, str]]
+
+    def __init__(self, url: str, characters: list[tuple[int, str]]) -> None:
+        self.url = url
+        self.characters = characters
+
+
     @staticmethod
     def read(stream: SWFInputStream) -> Tag:
-        if stream.version < 1:
+        if stream.version < 5:
             raise ValueError("bad swf version")
         
-        raise NotImplementedError()
+        url = stream.readString()
+        
+        count = stream.readUI16()
+        characters: list[tuple[int, str]] = []
+        
+        for _ in range(count):
+            tag = stream.readUI16()
+            name = stream.readString()
+            characters.append((tag, name))
+
+        return ImportAssetsTag(url, characters)
 
 
     def write(self, stream: SWFOutputStream) -> None:
-        if stream.version < 1:
+        if stream.version < 5:
             raise ValueError("bad swf version")
         
-        raise NotImplementedError()
+        stream.writeString(self.url)
+
+        stream.writeUI16(len(self.characters))
+        for tag, name in self.characters:
+            stream.writeUI16(tag)
+            stream.writeString(name)
