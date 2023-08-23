fd93d60af98df5bd064bbf774f7a9fc778d32e51
NathaanTFM <56046513+NathaanTFM@users.noreply.github.com>
2023-08-23 19:20:41 +0200

    Ajout DefineScalingGridTag

--- swf/tags/DefineScalingGridTag.py
+++ swf/tags/DefineScalingGridTag.py
@@ -2,25 +2,37 @@ from __future__ import annotations
 from swf.tags.Tag import Tag
 from swf.stream.SWFInputStream import SWFInputStream
 from swf.stream.SWFOutputStream import SWFOutputStream
+from swf.records.Rectangle import Rectangle
 
 class DefineScalingGridTag(Tag):
     """
-    The ShowFrame tag instructs Flash Player to display the
-    contents of the display list. The file is paused for
-    the duration of a single frame.
+    The DefineScalingGrid tag introduces the concept of 9-slice scaling,
+    which allows component-style scaling to be applied
+    to a sprite or button character.
     """
     tagId = 78
 
+    characterId: int
+    splitter: Rectangle
+
+    def __init__(self, characterId: int, splitter: Rectangle) -> None:
+        self.characterId = characterId
+        self.splitter = splitter
+        
+
     @staticmethod
     def read(stream: SWFInputStream) -> Tag:
-        if stream.version < 1:
+        if stream.version < 8:
             raise ValueError("bad swf version")
         
-        raise NotImplementedError()
+        characterId = stream.readUI16()
+        splitter = stream.readRECT()
+        return DefineScalingGridTag(characterId, splitter)
 
 
     def write(self, stream: SWFOutputStream) -> None:
-        if stream.version < 1:
+        if stream.version < 8:
             raise ValueError("bad swf version")
         
-        raise NotImplementedError()
+        stream.writeUI16(self.characterId)
+        stream.writeRECT(self.splitter)
